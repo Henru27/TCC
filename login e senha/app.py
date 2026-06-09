60d3fe85286c280fe095c77c424c3ac265ab213c
Vitor <vittin2302@gmail.com>
2026-06-09 11:45:47 -0300

    Add files via upload

--- login e senha/app.py
+++ login e senha/app.py
@@ -7,11 +7,30 @@ app = Flask(__name__)
 def login():
 
     if request.method == "POST":
+
         usuario = request.form["usuario"]
         senha = request.form["senha"]
 
-        return f"Usuário: {usuario} | Senha: {senha}"
+        conexao = sqlite3.connect("banco.db")
+        cursor = conexao.cursor()
 
-    return render_template("index.html")
+        print("usuario digitado", usuario)
+        print("senha digitado", senha)
+
+        cursor.execute(
+            "SELECT * FROM usuario WHERE nome = ? AND senha = ?",
+            (usuario, senha)
+        )
+
+        resultado = cursor.fetchone()
+
+        conexao.close()
+
+        if resultado:
+            return "Login realizado com sucesso!"
+        else:
+            return "Usuário ou senha incorretos."
+
+    return render_template("login.html")
 
 app.run(debug=True)
